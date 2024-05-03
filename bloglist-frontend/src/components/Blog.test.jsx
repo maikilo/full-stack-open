6c5774b2573de2ab945eeed498cf0bc627f0bc81
maikilo <93522897+maikilo@users.noreply.github.com>
2024-05-03 22:39:38 +0300

    5.16: Testing BlogForm

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -2,81 +2,80 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author but not blog details', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'me',
-    url: 'google.fi',
-    likes: 0,
-    user: { name: 'Paul', username: 'Paul', id: '283519765' }
-  }
+describe('<BlogForm />', () => {
+  test('renders title and author but not blog details', () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'me',
+      url: 'google.fi',
+      likes: 0,
+      user: { name: 'Paul', username: 'Paul', id: '283519765' }
+    }
 
-  render(<Blog blog={blog} />)
-  //screen.debug()
+    render(<Blog blog={blog} />)
+    //screen.debug()
 
-  const element = screen.getByTestId('blog')
-  expect(element).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-  expect(element).toHaveTextContent(
-    'me'
-  )
+    const element = screen.getByTestId('blog')
+    expect(element).toHaveTextContent(
+      'Component testing is done with react-testing-library'
+    )
+    expect(element).toHaveTextContent(
+      'me'
+    )
 
-  const togglableContent = screen.getByTestId('togglableContent')
-  expect(togglableContent).toHaveStyle(
-    'display: none'
-  )
+    const togglableContent = screen.getByTestId('togglableContent')
+    expect(togglableContent).toHaveStyle(
+      'display: none'
+    )
 
+  })
+
+  test('blog details rendered after user clicks view button', async () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'me',
+      url: 'google.fi',
+      likes: 0,
+      user: { name: 'Paul', username: 'Paul', id: '283519765' }
+    }
+
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const element = screen.getByTestId('togglableContent')
+    expect(element).toHaveTextContent(
+      'url: google.fi'
+    )
+    expect(element).toHaveTextContent(
+      'likes: 0'
+    )
+  })
+
+  test('Liking a blog twice', async() => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'me',
+      url: 'google.fi',
+      likes: 0,
+      user: { name: 'Paul', username: 'Paul', id: '283519765' }
+    }
+
+    const addLike = vi.fn()
+
+    render(<Blog blog={blog} addLike={addLike}/>)
+    const user = userEvent.setup()
+
+    const showButton = screen.getByText('view')
+    await user.click(showButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(addLike.mock.calls).toHaveLength(2)
+
+  })
 })
-
-test('blog details rendered after user clicks view button', async () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'me',
-    url: 'google.fi',
-    likes: 0,
-    user: { name: 'Paul', username: 'Paul', id: '283519765' }
-  }
-
-  render(<Blog blog={blog} />)
-
-  const user = userEvent.setup()
-  const button = screen.getByText('view')
-  await user.click(button)
-
-  const element = screen.getByTestId('togglableContent')
-  expect(element).toHaveTextContent(
-    'url: google.fi'
-  )
-  expect(element).toHaveTextContent(
-    'likes: 0'
-  )
-})
-
-test('Liking a blog twice', async() => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'me',
-    url: 'google.fi',
-    likes: 0,
-    user: { name: 'Paul', username: 'Paul', id: '283519765' }
-  }
-
-  const addLike = vi.fn()
-
-  render(<Blog blog={blog} addLike={addLike}/>)
-
-  const user = userEvent.setup()
-  const element = screen.getByTestId('togglableContent')
-
-  const showButton = screen.getByText('view')
-  await user.click(showButton)
-
-  const likeButton = screen.getByText('like')
-  await user.click(likeButton)
-  await user.click(likeButton)
-
-  expect(addLike.mock.calls).toHaveLength(2)
-
-})
-
--- bloglist-frontend/src/components/BlogForm.test.jsx
+++ bloglist-frontend/src/components/BlogForm.test.jsx
@@ -1,41 +1,26 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Blog from './Blog'
 import BlogForm from './BlogForm'
 
+describe('<BlogForm />', () => {
+  test('Posting a blog with BlogForm', async() => {
+    const postBlog = vi.fn()
+    render(<BlogForm postBlog={postBlog} />)
 
-test('Posting a blog with BlogForm', async() => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'me',
-    url: 'google.fi',
-    likes: 0,
-    user: { name: 'Paul', username: 'Paul', id: '283519765' }
-  }
+    const user = userEvent.setup()
+    const postButton = screen.getByText('save')
 
-  const postBlog = vi.fn(e => e.preventDefault())
-  const handleTitleChange = vi.fn()
-  const handleAuthorChange = vi.fn()
-  const handleUrlChange = vi.fn()
+    const inputs = screen.getAllByRole('textbox')
+    await user.type(inputs[0], 'testing a form...')
+    await user.type(inputs[1], 'Anna')
+    await user.type(inputs[2], 'https://ft.com')
 
-  render(<BlogForm
-    postBlog={postBlog}
-    handleTitleChange={handleTitleChange}
-    handleAuthorChange={handleAuthorChange}
-    handleUrlChange={handleUrlChange} />)
+    await user.click(postButton)
 
-  const user = userEvent.setup()
-  const postButton = screen.getByText('save')
+    expect(postBlog.mock.calls).toHaveLength(1)
+    expect(postBlog.mock.calls[0][0].title).toBe('testing a form...')
+    expect(postBlog.mock.calls[0][0].author).toBe('Anna')
+    expect(postBlog.mock.calls[0][0].url).toBe('https://ft.com')
 
-  const inputs = screen.getAllByRole('textbox')
-  await user.type(inputs[0], 'testing a form...')
-  await user.type(inputs[1], 'Anna')
-  await user.type(inputs[2], 'https://ft.com')
-
-  await user.click(postButton)
-
-  expect(postBlog.mock.calls).toHaveLength(1)
-  expect(postBlog.mock.calls[0][0].content).toBe('testing a form...')
-
+  })
 })
-
