80b181093c8ae374c1c649be6b490910fac71969
maikilo <93522897+maikilo@users.noreply.github.com>
2024-07-03 17:06:30 +0300

    6.6-6.7: Separate action creators and new component AnecdoteForm

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { addVote, addAnecdote } from './reducers/anecdoteReducer'
+import { addVote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,14 +9,6 @@ const App = () => {
   const vote = (id) => {
     console.log('vote', id)
     dispatch(addVote(id))
-  }
-
-  const createAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    console.log('new anecdote', content)
-    dispatch(addAnecdote(content))
-    event.target.anecdote.value = ''
   }
 
   return (
@@ -33,12 +26,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
-        <div>
-          <input name='anecdote' />
-        </div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,24 @@
+import { addAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+    const createAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        console.log('new anecdote', content)
+        dispatch(addAnecdote(content))
+        event.target.anecdote.value = ''
+    }
+
+    return (
+        <form onSubmit={createAnecdote}>
+            <div>
+                <input name='anecdote' />
+            </div>
+            <button type='submit'>create</button>
+        </form>
+    )
+}
+
+export default AnecdoteForm
