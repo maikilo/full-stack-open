d11b21f407e2c628ff4fd63dcb6d59ce246197f1
maikilo <93522897+maikilo@users.noreply.github.com>
2024-08-01 21:28:18 +0300

    7.14: Implement a view to the application that displays all of the basic information related to users

--- part7/bloglist-frontend/src/App.jsx
+++ part7/bloglist-frontend/src/App.jsx
@@ -1,21 +1,23 @@
 import { useState, useEffect } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
+
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
 import Togglable from './components/Togglable'
+import Users from './components/Users'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import { useNotificationValue, useNotificationDispatch } from './NotificationContext'
 import { useUserValue, useUserDispatch } from './UserContext'
 
 
-const App = () => {
+const Blogs = () => {
   const [blogFormVisible, setBlogFormVisible] = useState(false)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  //const [user, setUser] = useState(null)
 
   const notification = useNotificationValue()
   const notificationDispatch = useNotificationDispatch()
@@ -28,53 +30,6 @@ const App = () => {
   const setNotification = (payload) => {
     notificationDispatch({ payload, type: 'CREATE' })
     setTimeout(() => notificationDispatch({ type: 'CLEAR' }), 5000)
-  }
-
-  const newBlogMutation = useMutation({
-    mutationFn: blogService.create,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['blogs'] })
-    },
-    onError: () => {
-      setNotification( { message: 'Error in posting a blog', messageType: 'error' })
-    }
-  })
-
-  const likeBlogMutation = useMutation({
-    mutationFn: blogService.update,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['blogs'] })
-    },
-    onError: (blog) => {
-      setNotification( { message: `An error occured when liking ${blog.title}`, messageType: 'error' })
-    }
-  })
-
-  const deleteBlogMutation = useMutation({
-    mutationFn: blogService.deleteBlog,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['blogs'] })
-    },
-    onError: () => {
-      setNotification( { message: 'Error in deleting a blog', messageType: 'error' })
-    }
-  })
-
-  const addLike = id => {
-    const blog = blogs.find(b => b.id === id)
-    const updatedBlog = { ...blog, likes: blog.likes + 1, user: user.id }
-    likeBlogMutation.mutate(updatedBlog)
-  }
-
-  const deleteBlog = async (id) => {
-    const blog = blogs.find(b => b.id === id)
-    if (confirm(`Sure you want to delete ${blog.title}?`)) {
-      deleteBlogMutation.mutate(id)
-    }
-  }
-
-  const postBlog = async (blogObject) => {
-    newBlogMutation.mutate(blogObject)
   }
 
   useEffect(() => {
@@ -126,6 +81,53 @@ const App = () => {
         </Togglable>
       </div>
     )
+  }
+
+  const newBlogMutation = useMutation({
+    mutationFn: blogService.create,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['blogs'] })
+    },
+    onError: () => {
+      setNotification( { message: 'Error in posting a blog', messageType: 'error' })
+    }
+  })
+
+  const likeBlogMutation = useMutation({
+    mutationFn: blogService.update,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['blogs'] })
+    },
+    onError: (blog) => {
+      setNotification( { message: `An error occured when liking ${blog.title}`, messageType: 'error' })
+    }
+  })
+
+  const deleteBlogMutation = useMutation({
+    mutationFn: blogService.deleteBlog,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['blogs'] })
+    },
+    onError: () => {
+      setNotification( { message: 'Error in deleting a blog', messageType: 'error' })
+    }
+  })
+
+  const addLike = id => {
+    const blog = blogs.find(b => b.id === id)
+    const updatedBlog = { ...blog, likes: blog.likes + 1, user: user.id }
+    likeBlogMutation.mutate(updatedBlog)
+  }
+
+  const deleteBlog = async (id) => {
+    const blog = blogs.find(b => b.id === id)
+    if (confirm(`Sure you want to delete ${blog.title}?`)) {
+      deleteBlogMutation.mutate(id)
+    }
+  }
+
+  const postBlog = async (blogObject) => {
+    newBlogMutation.mutate(blogObject)
   }
 
   const blogForm = () => {
@@ -186,8 +188,30 @@ const App = () => {
       }
     </div>
   )
+}
 
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+      <Link style={padding} to="/">Blogs</Link>
+      <Link style={padding} to="/users">Users</Link>
+    </div>
+  )
+}
 
+const App = () => {
+  return (
+    <div>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Blogs />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
--- part7/bloglist-frontend/src/main.jsx
+++ part7/bloglist-frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NotificationContextProvider } from './NotificationContext'
 import { UserContextProvider } from './UserContext'
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <UserContextProvider>
       <NotificationContextProvider>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </NotificationContextProvider>
     </UserContextProvider>
   </QueryClientProvider>
